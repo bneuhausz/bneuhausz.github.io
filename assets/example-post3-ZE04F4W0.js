e92be0649e37c8f47caf1b7dceea5ca723231367
bneuhausz <bneuhausz@users.noreply.github.com>
2025-05-17 11:14:49 +0000

    deployed via Analog Publish Github Pages 🚀 for 432fba463c95a6cfa754806cbf82e36d3df4ddb5

---
+++ assets/example-post3-ZE04F4W0.js
@@ -0,0 +1,12 @@
+const e=`---
+title: Example title
+slug: example-post
+description: Example description
+coverImage: https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80
+coverImageDescription: example cover image
+tags: [tag1, tag2]
+shadowColor: default
+---
+
+<h1 id="example-blog-post-2">Example Blog Post</h1>
+<p>This is an example blog post written in markdown!</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`;export{e as default};
