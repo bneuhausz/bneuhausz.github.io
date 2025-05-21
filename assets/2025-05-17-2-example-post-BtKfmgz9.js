2826f64496e79a61d23c3b979d76d22fb9174980
bneuhausz <bneuhausz@users.noreply.github.com>
2025-05-21 11:56:13 +0000

    deployed via Analog Publish Github Pages 🚀 for 6c264e914ade7738d85d5212ae459d6b85525723

---
+++ assets/2025-05-17-2-example-post-BtKfmgz9.js
@@ -0,0 +1,14 @@
+const e=`---
+title: Example title 2
+slug: 2025-05-17-2-example-post
+description: Example description
+coverImage: /images/angular_wordmark_gradient.png
+coverImageDescription: example cover image
+thumbnail: /images/dotnet_resized_thumbnail.png
+thumbnailDescription: example thumbnail image
+tags: [tag1, tag2]
+shadowColor: dotnet
+---
+
+<h1 id="example-blog-post-2">Example Blog Post 2</h1>
+<p>This is an example blog post written in markdown!</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`;export{e as default};
