b61745b5077d27e3719348f9dbbde3fcf5945ada
bneuhausz <bneuhausz@gmail.com>
2025-08-16 22:15:31 +0200

    some more seo stuff

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,7 +1,12 @@
+import { RouteMeta } from '@analogjs/router';
 import { Component } from '@angular/core';
+
+export const routeMeta: RouteMeta = {
+  title: 'bneuhausz.dev',
+};
 
 @Component({
   standalone: true,
   template: ` <h2>Welcome</h2> `,
 })
-export default class HomePageComponent {}
+export default class HomePageComponent { }
