14b2d5195f6f91e2b766949d1d450a6ae87158e1
bneuhausz <bneuhausz@gmail.com>
2025-08-20 14:59:11 +0200

    some design changes

---
+++ src/app/shared/directives/external-links.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, ElementRef, AfterViewInit, Renderer2, inject } from '@angular/core';
+import { isBrowser } from '../utils/browser.utils';
+
+@Directive({
+  selector: '[appExternalLinks]',
+})
+export class ExternalLinksDirective implements AfterViewInit {
+  private readonly el = inject(ElementRef);
+  private readonly renderer = inject(Renderer2);
+
+  ngAfterViewInit(): void {
+    if (!isBrowser()) return;
+
+    const observer = new MutationObserver(() => {
+      const anchorElements = this.el.nativeElement.querySelectorAll('a');
+      if (anchorElements.length === 0) return;
+
+      anchorElements.forEach((a: HTMLAnchorElement) => {
+        if (a.hostname !== window.location.hostname) {
+          this.renderer.setAttribute(a, 'target', '_blank');
+          this.renderer.setAttribute(a, 'rel', 'noopener noreferrer');
+        }
+      });
+
+      observer.disconnect();
+    });
+
+    observer.observe(this.el.nativeElement, {
+      childList: true,
+      subtree: true,
+    });
+  }
+}
