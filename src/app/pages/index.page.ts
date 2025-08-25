fca25dec3a3568e5c9cb7afaeb7142c619ffdeb5
bneuhausz <bneuhausz@gmail.com>
2025-08-25 16:43:35 +0200

    addedn subscribe to newsletter feature

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -22,6 +22,17 @@ export const routeMeta: RouteMeta = {
 
       <div class="card w-96 bg-neutral text-amber-100 card-xl shadow-sm">
         <div class="card-body items-center">
+          <h2 class="card-title">Newsletter</h2>
+          <p>Sign up for my - probably - monthly newsletter to receive updates about new posts and possibly other content</p>
+          <div class="card-actions">
+            <!-- <a class="btn btn-ghost hover:bg-amber-100 hover:text-amber-900" routerLink="/newsletter">Read More</a> -->
+            <button class="ml-onclick-form btn btn-ghost hover:bg-amber-100 hover:text-amber-900" onclick="ml('show', 'QIrGIF', true)">Subscribe</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="card w-96 bg-neutral text-amber-100 card-xl shadow-sm">
+        <div class="card-body items-center">
           <h2 class="card-title">About me</h2>
           <p>You can read a bit about me here</p>
           <div class="card-actions">
